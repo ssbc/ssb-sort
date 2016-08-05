859f230b02011e059b928c9f7d98e9db05cbff10
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-05 17:50:47 +1200

    sort correctly!

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function links (obj, each) {
   if(isMsg(obj)) return each(obj)
   if(!obj || 'object' !== typeof obj) return
   for(var k in obj)
-    links(obj[k], each)
+    if(links(obj[k], each)) return true
 }
 
 function firstKey (obj) {
   for(var k in obj) return k
 }
 
-//used to initialize sort
+//get object representing messages in thread.
 function messages (thread) {
   var counts = {}
 
@@ -25,11 +25,39 @@ function messages (thread) {
   return counts
 }
 
+function keys (thread) {
+  var o = {}
+  thread.forEach(function (e) {
+    o[e.key] = e.value
+  })
+  return o
+}
+
+function isString(s) { return 'string' === typeof s }
+
+function ancestors(thread, start, each) {
+  if(Array.isArray(thread))
+    thread = keys(thread)
+  start = isString(start) ? start : start.key
+  var seen = {}
+  function traverse (key) {
+    if(seen[key]) return
+    var ret
+    seen[key] = true
+    if(each(thread[key], key)) return true
+    return links(thread[key], function (link) {
+      if(thread[link]) return traverse(link)
+    })
+  }
+
+  return traverse(start)
+}
+
+//messages in thread that are not referenced by another message in the thread.
 function heads (thread) {
 
   var counts = messages(thread)
   thread.forEach(function (msg) {
-    if(counts[msg.key] == 0) return
     links(msg.value, function (link) {
         change = true
       counts[link] = 0
@@ -37,7 +65,7 @@ function heads (thread) {
   })
   var ary = []
   for(var k in counts) if(counts[k] !== 0) ary.push(k)
-  return ary
+  return ary.sort()
 }
 
 function roots (thread) {
@@ -55,32 +83,22 @@ function roots (thread) {
   return ary
 }
 
+function sort (thread) {
+  var obj = keys(thread)
 
-function order (thread) {
-  var counts = messages(thread)
-  //until(function () {
-  var ordered = true
-  while(ordered) {
-    ordered = false
-    thread.forEach(function (msg) {
-      var max = counts[msg.key]
-      links(msg.value, function (e) {
-        if(counts[e] && counts[e] + 1 > max) max = counts[e] + 1
-      })
-      if(max > counts[msg.key]) {
-        ordered = true
-        counts[msg.key] = max
-      }
+  function ancestorOf(a, b) {
+    return ancestors(obj, a.key, function (_b, k) {
+      return b.key === k
     })
   }
-  return counts
-}
 
-function sort (thread) {
-  var counts = order(thread)
+  function compare(a, b) {
+    return ancestorOf(a, b) ? 1 : ancestorOf(b, a) ? -1 : 0
+  }
+
   return thread.sort(function (a, b) {
     return (
-      counts[a.key] - counts[b.key]
+      compare(a, b)
       //received timestamp, may not be present
       || a.timestamp - b.timestamp
       //declared timestamp, may by incorrect or a lie
@@ -93,13 +111,6 @@ function sort (thread) {
 
 exports = module.exports = sort
 exports.heads = heads
-exports.order = order
 exports.roots = roots
-
-
-
-
-
-
-
+exports.ancestors = ancestors
 

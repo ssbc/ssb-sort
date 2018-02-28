fa284353bc75e5ab8901a1efc96ec384043b32e8
mix irving <whimful@gmail.com>
2018-02-28 23:08:40 +1300

    manually copy in 1.0.0

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
 var isMsgRef = require('ssb-ref').isMsg
 
-function links (obj, each) {
-  if(isMsgRef(obj)) return each(obj)
-  if(!obj || 'object' !== typeof obj) return
-  for(var k in obj)
-    links(obj[k], each)
-}
-
-//used to initialize sort
-function messages (thread) {
-  var counts = {}
-
-  for(var i = 0; i < thread.length; i++) {
-    var key = thread[i].key
-    if(counts[key])
-      throw new Error('thread has duplicate message:'+key)
-    counts[key] = 1
-  }
-
-  return counts
-}
-
+//messages in thread that are not referenced by another message in the thread.
 function heads (thread) {
-  sort(thread)
   var counts = messages(thread)
 
   thread.forEach(function (msg) {
-    if(counts[msg.key] == 0) return
     links(msg.value, function (link) {
+        change = true
       counts[link] = 0
     })
   })
   var ary = []
   for(var k in counts) if(counts[k] !== 0) ary.push(k)
-  return ary
+  return ary.sort()
 }
 
 function roots (thread) {
-  sort(thread)
+  // sort(thread)
   var counts = messages(thread)
 
   thread.forEach(function (msg) {
@@ -50,7 +29,6 @@ function roots (thread) {
   for(var k in counts) if(counts[k] === 1) ary.push(k)
   return ary
 }
-
 
 function order (thread) {
   var counts = messages(thread)
@@ -74,12 +52,22 @@ function order (thread) {
   return counts
 }
 
-function sort (thread, _order) {
-  if(!_order) _order = order(thread)
+function sort (thread) {
+  var obj = arrayToDict(thread)
+
+  function ancestorOf(a, b) {
+    return ancestors(obj, a.key, function (_b, k) {
+      return b.key === k
+    })
+  }
+
+  function compare(a, b) {
+    return ancestorOf(a, b) ? 1 : ancestorOf(b, a) ? -1 : 0
+  }
 
   return thread.sort(function (a, b) {
     return (
-      _order[a.key] - _order[b.key]
+      compare(a, b)
       //received timestamp, may not be present
       || a.timestamp - b.timestamp
       //declared timestamp, may by incorrect or a lie
@@ -90,9 +78,27 @@ function sort (thread, _order) {
   })
 }
 
+function ancestors (thread, start, each) {
+  if(Array.isArray(thread))
+    thread = arrayToDict(thread)
+  start = isString(start) ? start : start.key
+  var seen = {}
+  function traverse (key) {
+    if(seen[key]) return
+    var ret
+    seen[key] = true
+    if(each(thread[key], key)) return true
+    return links(thread[key], function (link) {
+      if(thread[link]) return traverse(link)
+    })
+  }
+
+  return traverse(start)
+}
+
 function missingContext (thread) {
-  var _order = order(thread)
-  sort(thread, _order) // save a double order
+  var counts = order(thread)
+  sort(thread)
 
   var missingContext = {}
 
@@ -101,7 +107,7 @@ function missingContext (thread) {
   var done = false
   while (!done) {
     subset = thread.filter(function (msg) {
-      return _order[msg.key] <= depth
+      return counts[msg.key] <= depth
     })
     var subsetHeads = heads(subset)
 
@@ -129,4 +135,40 @@ exports = module.exports = sort
 exports.heads = heads
 exports.order = order
 exports.roots = roots
+exports.ancestors = ancestors
 exports.missingContext = missingContext
+
+
+// Utils ///////////////////////////////////
+
+function links (obj, each) {
+  if(isMsgRef(obj)) return each(obj)
+  if(!obj || 'object' !== typeof obj) return
+  for(var k in obj)
+    if(links(obj[k], each)) return true
+}
+
+//used to initialize sort
+function messages (thread) {
+  var counts = {}
+
+  for(var i = 0; i < thread.length; i++) {
+    var key = thread[i].key
+    if(counts[key])
+      throw new Error('thread has duplicate message:'+key)
+    counts[key] = 1
+  }
+
+  return counts
+}
+
+function arrayToDict (thread) {
+  var o = {}
+  thread.forEach(function (e) {
+    o[e.key] = e.value
+  })
+  return o
+}
+
+function isString(s) { return 'string' === typeof s }
+
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,6 @@
 var tape = require('tape')
+var fs = require('fs')
+var path = require('path')
 var sort = require('../')
 var kv = require('./fakeMsg')
 var shuffle = require('./shuffle')
@@ -125,3 +127,38 @@ tape('multiple merge', function (t) {
 
   t.end()
 })
+
+tape('real', function (t) {
+  var thread = fs.readFileSync(path.join(__dirname, 'thread.json'), 'utf8').split('\n\n').filter(Boolean).map(JSON.parse)
+
+  var thread2 = sort(thread.slice())
+  var thread3 = thread.slice().sort(function () { return Math.random() - 0.5 })
+  var h1 = sort.heads(thread)
+  var r1 = sort.roots(thread)
+  var h2 = sort.heads(thread2)
+  var r2 = sort.roots(thread2)
+  var h3 = sort.heads(thread3)
+  var r3 = sort.roots(thread3)
+
+  t.deepEqual(h1, h2)
+  t.deepEqual(h2, h3)
+  t.deepEqual(h1, h3)
+
+  t.deepEqual(r1, r2)
+  t.deepEqual(r2, r3)
+  t.deepEqual(r1, r3)
+
+  var sorted = sort(thread)
+  t.equal(sorted[0].key, r1[0])
+  var a = []
+  sort.ancestors(thread, h1[0], function (e, k) {
+    a.push(k)
+  })
+  var b = []
+  sort.ancestors(thread, h1[1], function (e, k) {
+    b.push(k)
+  })
+  // console.log(a, b)
+  t.end()
+})
+
